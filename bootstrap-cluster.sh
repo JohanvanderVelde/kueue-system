0884d551ef5980ec8ea5e542c656cd70250de147
J.van.der.Velde <18574994+JohanvanderVelde@users.noreply.github.com>
2026-03-03 16:31:26 +0100

    init release

---
+++ bootstrap-cluster.sh
@@ -0,0 +1,34 @@
+# install kind cluster
+kind create cluster --name localdev
+
+# install cert-manager
+helm install \
+  cert-manager oci://quay.io/jetstack/charts/cert-manager \
+  --version v1.19.2 \
+  --namespace cert-manager \
+  --create-namespace \
+  --set crds.enabled=true
+
+# install prometheus
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+helm repo add argo https://argoproj.github.io/argo-helm
+helm repo update
+helm install kube-prometheus-stack prometheus-community/kube-prometheus-stack \
+  --version 82.2.1 \
+  --namespace monitoring \
+  --create-namespace \
+  -f ./monitoring-values.yaml \
+  --wait --timeout 300s
+
+# install argocd
+helm install argocd argo/argo-cd \
+  --namespace argocd \
+  --create-namespace \
+  --wait --timeout 300s
+
+# install kueue
+helm install kueue oci://registry.k8s.io/kueue/charts/kueue \
+  --version=0.16.1 \
+  --namespace kueue-system \
+  -f ./values.yaml \
+  --wait --timeout 300s
